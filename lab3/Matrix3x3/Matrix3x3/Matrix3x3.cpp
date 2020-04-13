498081b8c80b72016b1d4581c14c6ffbff07cf64
marinaKadochnikova <63494477+marinaKadochnikova@users.noreply.github.com>
2020-04-13 21:39:57 +0500

    Исправление ошибок 13.04.2020
    Исправлено замечание : исключить привязку в методах к размеру 3х3, внесла константу

--- lab3/Matrix3x3/Matrix3x3/Matrix3x3.cpp
+++ lab3/Matrix3x3/Matrix3x3/Matrix3x3.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 class matrix3x3 {
-    int matrix[3][3];
+    static const int N = 3;
+    int matrix[N][N];
     int size;
 
 public:
     matrix3x3()
     {
-        size = 3;
+        size = N;
         for (int i = 0; i < size; i++)
             for (int j = 0; j < size; j++)
                 matrix[i][j] = 0;
@@ -30,16 +31,18 @@ public:
         matrix[i][j] = value;
     }
 
+    int GetSize() { return size; }
+
     void fillRandomElements(const int minVal, const int maxVal)
     {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
                 matrix[i][j] = rand() % (minVal - maxVal) - maxVal;
     }
     void Print()
     {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++)
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
                 cout << matrix[i][j] << '\t';
             cout << '\n';
         }
@@ -49,7 +52,7 @@ public:
     {
         int i, j;
         int sum = 0;
-        for (i = 0, j = 0; i < 3; i++, j++)
+        for (i = 0, j = 0; i < N; i++, j++)
             sum += matrix[i][j];
         return sum;
     }
@@ -58,7 +61,7 @@ public:
     {
         int sum = 0;
         int i, j;
-        for (i = 0, j = 2; i < 3; i++, j--)
+        for (i = 0, j = N - 1; i < N; i++, j--)
             sum += matrix[i][j];
         return sum;
     }
@@ -67,7 +70,7 @@ public:
     {
         int i, j;
         int product = 1;
-        for (i = 0, j = 0; i < 3; i++, j++)
+        for (i = 0, j = 0; i < N; i++, j++)
             product *= matrix[i][j];
         return product;
     }
@@ -76,7 +79,7 @@ public:
     {
         int i, j;
         int product = 1;
-        for (i = 0, j = 2; i < 3; i++, j--)
+        for (i = 0, j = N - 1; i < N; i++, j--)
             product *= matrix[i][j];
         return product;
     }
@@ -84,7 +87,7 @@ public:
     int sumRow(const int iRow)const
     {
         int sum = 0;
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < N; j++)
             sum += matrix[iRow][j];
         return sum;
     }
@@ -92,7 +95,7 @@ public:
     int minColumn(const int iCol) const
     {
         int min = matrix[0][0];
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < N; i++)
             if (matrix[i][iCol] < min)
                 min = matrix[i][iCol];
         return min;
@@ -101,7 +104,7 @@ public:
     int maxColumn(const int iCol)const
     {
         int max = matrix[0][0];
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < N; i++)
             if (matrix[i][iCol] > max)
                 max = matrix[i][iCol];
         return max;
@@ -114,17 +117,17 @@ int main()
     matrix3x3 a;
     a.fillRandomElements(-10, 9);
     a.Print();
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < a.GetSize(); i++)
         cout << "sum row " << i + 1 << " = " << a.sumRow(i) << '\n';
 
     cout << '\n';
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < a.GetSize(); i++)
         cout << "min column " << i + 1 << " = " << a.minColumn(i) << '\n';
 
     cout << '\n';
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < a.GetSize(); i++)
         cout << "max column " << i + 1 << " = " << a.maxColumn(i) << '\n';
 
     cout << '\n';
